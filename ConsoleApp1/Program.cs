f9ec9825adcb51e771c093a33fb15ca93838feec
Cameron Harris <cameron@cameronharris.org>
2021-01-25 13:02:16 +0000

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+#nullable disable
+
+    public class Foo
+    {
+        public DateTime Date { get; } = new(2020, 1, 1);
+
+        public static Foo Get()
+        {
+            return null;
+        }
+    }
+
+#nullable enable
+
+    public static class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine(new Foo()); // eliminate unused error
+            
+            var f = Foo.Get();
+
+            if (f?.Date != default && f.Date > DateTime.Now)
+            {
+                Console.WriteLine("X");
+            }
+        }
+    }
+}
